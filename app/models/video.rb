9a1e870a896093368e40b0f9c27bd101f179db4b
Santiago Ramos <sramos@sitiodistinto.net>
2011-12-20 10:15:29 +0100

    Probando clases de modelo en video

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,12 +5,12 @@ class Video < ActiveRecord::Base
   validates_presence_of :channel_id
 
   # Test para ejecutar desde un scheduller
-  class << self
-    def check_thumbnails
-      Video.all.each do |video|
-        puts("Video con id " + video.id.to_s) if video.thumbnail.nil?
-      end
-    end
-  end
+  #class << self
+  #  def check_thumbnails!
+  #    Video.all.each do |video|
+  #      puts("Video con id " + video.id.to_s) if video.thumbnail.nil?
+  #    end
+  #  end
+  #end
 
 end
